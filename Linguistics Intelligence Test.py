d0686d6ae9bef0eaccfeb2f2d3d6a7f876537f1c
Erika Siao <169622133+esiao125@users.noreply.github.com>
2024-09-21 14:41:29 -0700

    Adding the code for an intelligence test to execute

---
+++ Linguistics Intelligence Test.py
@@ -0,0 +1,360 @@
+#Erika Siao
+#February 7th 2024
+#Codedex
+
+Score=0
+
+q1=int(input('''Choose the word that best completes the sentence: A mellifluous tune is
+1) Smooth
+2) Loud
+3) Harsh
+4) I don’t know '''))
+if q1==1:
+  Score=Score+4
+elif q1==2:
+  Score=Score+3
+elif q1==3:
+  Score=Score+2
+elif q1==4:
+  Score=Score+1
+else:
+  print("Wrong input.")
+
+q2=int(input('''Choose the word that best completes the sentence: A fallible person is
+1) Flawed
+2) Prone to error
+3) Reliable
+4) I don’t know'''))
+if q2==1:
+  Score=Score+3
+elif q2==2:
+  Score=Score+4
+elif q2==3:
+  Score=Score+2
+elif q2==4:
+  Score=Score+1
+else:
+  print("Wrong input")
+
+q3=int(input('''Choose the word that best completes the sentence: An assiduous person is 
+1) Negligent
+2) Diligent
+3) Industrious
+4) I don’t know'''))
+if q3==1:
+  Score=Score+2
+elif q3==2:
+  Score=Score+4
+elif q3==3:
+  Score=Score+3
+elif q3==4:
+  Score=Score+1
+else:
+  print("Wrong input")
+
+q4=int(input('''When talking with a superior you should address them with a tone of
+1) Respect
+2) Annoyance
+3) Facetiousness
+4) I don’t know'''))
+if q4==1:
+  Score=Score+4
+elif q4==2:
+  Score=Score+2
+elif q4==3:
+  Score=Score+3
+elif q4==4:
+  Score=Score+1
+else:
+  print("Wrong input")
+
+q5=int(input('''What is the first rhetorical device in “the flowers in the garden bloomed like a vibrant tapestry attracting herds of wildlife”
+1) Personification
+2) Metaphor
+3) Simile
+4) I don’t know'''))
+if q5==1:
+  Score=Score+3
+elif q5==2:
+  Score=Score+2
+elif q5==3:
+  Score=Score+4
+elif q5==4:
+  Score=Score+1
+else:
+  print("Wrong input")
+
+q6=int(input('''What is the rhetorical device in “CDC recommends that everyone stay up to date on their COVID-19 vaccines, especially people with weakened immune systems.”
+1) Appeals to ethos
+2) Appeals to logos
+3) Appeals to pathos
+4) I don’t know'''))
+if q6==1:
+  Score=Score+4
+elif q6==2:
+  Score=Score+3
+elif q6==3:
+  Score=Score+2
+elif q6==4:
+  Score=Score+1
+else:
+  print("Wrong input")
+
+q7=int(input('''A indolent person is not one who
+1) Helps others
+2) Is lazy
+3) Is responsible
+4) I don’t know'''))
+if q7==1:
+  Score=Score+3
+elif q7==2:
+  Score=Score+2
+elif q7==3:
+  Score=Score+4
+elif q7==4:
+  Score=Score+1
+else:
+  print("Wrong input")
+
+q8=int(input('''A subtle difference is not
+1) Unseen
+2) Conspicuous
+3) Nuanced
+4) I don’t know'''))
+if q8==1:
+  Score=Score+3
+elif q8==2:
+  Score=Score+4
+elif q8==3:
+  Score=Score+2
+elif q8==4:
+  Score=Score+1
+else:
+  print("Wrong input")
+
+q9=int(input('''What is the tone of this dialogue: Interesting attempt, though a bit elementary for our academic standards.
+1) Cruel
+2) Dismissive
+3) Judgemental
+4) I don’t know'''))
+if q9==1:
+  Score=Score+2
+elif q9==2:
+  Score=Score+4
+elif q9==3:
+  Score=Score+3
+elif q9==4:
+  Score=Score+1
+else:
+  print("Wrong input")
+
+q10=int(input('''What is the change in tone in the following passage: 
+“Individually, we adore and pamper our children. We shuttle them from soccer practice to music lessons and then organize their play dates with meticulous fanaticism.
+
+Yet collectively, we mistreat America’s children, especially by the standards of other wealthy countries. When we’re formulating policies for children as a whole rather than coddling our own little angels, we fall scandalously short. We prize children in the abstract but as a society tend to ignore their needs.”
+1) Positive to negative
+2) Affectionate to critical
+3) Warm to disapproving
+4) I don’t know'''))
+if q10==1:
+  Score=Score+2
+elif q10==2:
+  Score=Score+4
+elif q10==3:
+  Score=Score+3
+elif q10==4:
+  Score=Score+1
+else:
+  print("Wrong input")
+
+q11=int(input('''Ana found herself walking down a windy pathway in the middle of the woods. __ a howl pierced the night that was answered by multiple howls throughout the woods, requesting reinforcements.
+What word best fits the sentence?
+1) When
+2) Suddenly
+3) While
+4) I don’t know'''))
+if q11==1:
+  Score=Score+2
+elif q11==2:
+  Score=Score+4
+elif q11==3:
+  Score=Score+3
+elif q11==4:
+  Score=Score+1
+else:
+  print("Wrong input")
+
+q12=int(input('''Through the rain and across the dirt raced a(n)  ___ rabbit excited to visit family and munch on many greens.
+What word best fits the sentence?
+1) Despotic
+2) Animated
+3) Feeble
+4) I don’t know'''))
+if q12==1:
+  Score=Score+3
+elif q12==2:
+  Score=Score+4
+elif q12==3:
+  Score=Score+2
+elif q12==4:
+  Score=Score+1
+else:
+  print("Wrong input")
+
+q13=int(input('''Cave paintings were discovered in Altamira and Lascaux in 1879 and ___
+Choose the answer that is grammatically correct.
+1) 1940; respectively.
+2) 1940 respectively.
+3) 1940, respectively.
+4) I don’t know'''))
+if q13==1:
+  Score=Score+2
+elif q13==2:
+  Score=Score+3
+elif q13==3:
+  Score=Score+4
+elif q13==4:
+  Score=Score+1
+else:
+  print("Wrong input")
+
+q14=int(input('''Over winter break Sally and her family traveled to New York where they saw a Broadway play that entranced them so much that they had no idea three hours had passed.
+Choose the answer that is grammatically correct.
+1) Over winter break Sally and her family traveled to New York, where they saw a Broadway play that entranced them so much that they had no idea three hours had passed.
+2) Over winter break, Sally and her family traveled to New York where they saw a Broadway play that entranced them so much that they had no idea three hours had passed.
+3) Over winter break, Sally and her family traveled to New York, where they saw a Broadway play that entranced them so much that they had no idea three hours had passed.
+4) I don’t know.'''))
+if q14==1:
+  Score=Score+2
+elif q14==2:
+  Score=Score+3
+elif q14==3:
+  Score=Score+4
+elif q14==4:
+  Score=Score+1
+else:
+  print("Wrong input")
+
+q15=int(input('''The internet is rewiring people’s brains, making them more impatient and unable to focus. Studies have shown that the easy access of the internet has shortened people’s attention span and reduced their comprehension. Despite these immense and significant worries cultivated from the rewiring of the brain, many are still satisfied with the trade.
+What is the author’s point?
+1) The internet is bad for people.
+2) The internet is rewiring people’s brains.
+3) The internet is satisfying for people.
+4) I don’t know'''))
+if q15==1:
+  Score=Score+3
+elif q15==2:
+  Score=Score+4
+elif q15==3:
+  Score=Score+2
+elif q15==4:
+  Score=Score+1
+else:
+  print("Wrong input")
+
+q16=int(input('''Forty-five cats! Forty-five cats all over the place, spreading hair and clawing up the furniture! They are ruining the house ___ Alex refuses to do something about this situation! It is very unsanitary and illegal. Oh, why? Oh, why? Please, why me?
+What word best completes the sentence?
+1) Yet
+2) Although
+3) Nonetheless
+4) I don’t know'''))
+if q16==1:
+  Score=Score+4
+elif q16==2:
+  Score=Score+2
+elif q16==3:
+  Score=Score+3
+elif q16==4:
+  Score=Score+1
+else:
+  print("Wrong input")
+
+q17=int(input('''The train raced through the countryside, bringing relief and many passengers to their intended destinations. The platforms held hundreds of rushing people laden with bulging cases, jostling each other while attempting to reach their target. The clock struck nine, coordinating with the trains’ whistle that signaled the last call for departure. People bolted for the exits, away from the train spewing fire.
+What type of rhetorical device is present at the end of the paragraph?
+1) Personification
+2) Anaphora
+3) Paraprosdokian
+4) I don’t know'''))
+if q7==1:
+  Score=Score+3
+elif q17==2:
+  Score=Score+2
+elif q17==3:
+  Score=Score+4
+elif q17==4:
+  Score=Score+1
+else:
+  print("Wrong input")
+
+q18=int(input('''
+Inspiration continues to evade me.
+Words drift about my mind’s endless sea,
+forming nonsense that I did not request.
+All day and all night, I try and try to fix this mess,
+but I can not write a single word.
+What is the placement of the rhyme in the poem?
+1) Head rhyme
+2) Close rhyme
+3) Terminal rhyme
+4) I don’t know'''))
+if q18==1:
+  Score=Score+2
+elif q18==2:
+  Score=Score+3
+elif q18==3:
+  Score=Score+4
+elif q18==4:
+  Score=Score+1
+else:
+  print("Wrong input")
+
+q19=int(input('''
+As the sleepy serene sun wakes,
+a spotlight shines on the crystal lakes,
+bouncing off the lakes’ fold
+and onto the leaves of red and gold.
+
+Creeping into the darkest voids,
+where the troublesome avoids,
+stirring the peaceful souls
+from their most tranquil holes.
+
+Drowsy critters rouses
+from deep within their houses,
+stumbling and staggering toward
+the gathering horde.
+What is the rhyme scheme?
+1) Single
+2) Cross
+3) Opposed
+4) I don’t know
+'''))
+if q19==1:
+  Score=Score+2
+elif q19==2:
+  Score=Score+3
+elif q19==3:
+  Score=Score+4
+elif q19==4:
+  Score=Score+1
+else:
+  print("Wrong input")
+
+q20=int(input('''The girl rode her bike. She hoped to beat her brother home, so she strayed from the path down an alley that cut across town. __ when she arrived her brother had beaten her there. He stood triumphantly at the door with his bike. Suddenly, their father spoke, “I’m impressed, Jen. You almost beat your brother and me home in the car. Great job!”
+What word best fits the sentence according to standard English?
+1) In spite of her efforts
+2) However
+3) Though
+4) I don’t know'''))
+if q20==1:
+  Score=Score+2
+elif q20==2:
+  Score=Score+4
+elif q20==3:
+  Score=Score+3
+elif q20==4:
+  Score=Score+1
+else:
+  print("Wrong input")
+
+print(Score)
